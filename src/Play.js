183238044813e5bb5045cf00c2ebfac69643c535
“Nicolas <ncorfmat@ucsc.edu>
2024-02-02 18:50:24 -0800

    Disabled physics debug mode

--- src/Play.js
+++ src/Play.js
@@ -112,11 +112,12 @@ class Play extends Phaser.Scene {
         }
     }
 }
+
 /*
 CODE CHALLENGE
 Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
-[ ] Add ball reset logic on successful shot
-[ ] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
-[ ] Make one obstacle move left/right and bounce against screen edges
-[ ] Create and display shot counter, score, and successful shot percentage
+[ DONE ] Add ball reset logic on successful shot
+[ DONE ] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
+[ DONE ] Make one obstacle move left/right and bounce against screen edges
+[ DONE ] Create and display shot counter, score, and successful shot percentage
 */
